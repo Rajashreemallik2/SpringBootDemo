7c1d6287d814ffaed1390ebfea631e95586dfcfe
Rajashreemallik2 <rajashreemallik2@gmail.com>
2024-04-15 00:33:11 +0530

    Building a springBoot web app with JPA Entities

---
+++ src/test/java/com/example/demo/springframework/DemoSpringframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
